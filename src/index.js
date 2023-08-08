617965986781424c9c3848d77e78c360c5548b49
jcmunav63 <jcmunav63@gmail.com>
2023-08-08 14:03:12 -0600

    Add linters files, README, LICENSE, config and dist folder.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
+function component1() {
+  const heading = document.createElement('h1');
+  heading.innerHTML = 'Hello Webpack & Gitflow!';
+  heading.classList.add('hello');
+  return heading;
+}
+
+function component2() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
   element.classList.add('hello');
 
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component1());
+document.body.appendChild(component2());
